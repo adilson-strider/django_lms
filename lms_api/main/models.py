a19fcbcebfc513cb2046affd15de0415cfb24df2
adilson-strider <adilsonjunior.dev01@gmail.com>
2023-06-06 11:30:49 -0300

    first commit

---
+++ lms_api/main/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Modelo Professor
+class Teacher(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    qualification=models.CharField(max_length=200)
+    mobile_no=models.CharField(max_length=20)
+    address=models.TextField()
+
+    class Meta:
+        verbose_name_plural="1. Teachers"
+
+# Modelo Categoria de Curso
+class CourseCategory(models.Model):
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+
+    class Meta:
+        verbose_name_plural="2. Course Categories"
+
+# Modelo de Curso
+class Course(models.Model):
+    category=models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    teacher=models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+
+    class Meta:
+        verbose_name_plural="3. Courses"
+
+# Modelo Estudante
+class Student(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    qualification=models.CharField(max_length=200)
+    mobile_no=models.CharField(max_length=20)
+    address=models.TextField()
+    interest_categories=models.TextField()
+
+    class Meta:
+        verbose_name_plural="4. Students"
+  
